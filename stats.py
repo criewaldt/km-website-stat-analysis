c375744c69123f5e04e25a47fc6f1aa063832a43
Christopher D. Riewaldt <c.riewaldt@gmail.com>
2023-03-13 17:39:42 -0400

    updates

--- stats.py
+++ stats.py
@@ -1,63 +1,79 @@
-import json, csv
-from opendata.opendata import GetDataByJOB
+import json, csv, datetime
 
-job_data = GetDataByJOB()
+#load INTERNAL data
+internal_jobs_list=[]
+with open('input/job_data.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        if (row[2]):
+            internal_jobs_list.append(row[2])
+print('Internal jobs for processing:', len(internal_jobs_list))
+# END INTERNAL DATA
 
-#load jobs data into list
-csv_path = 'input/jobs.csv'
-gathered_jobs_list = []
-jobs_list=[]
-
+# Load the OPENDATA data
 now_jobs = []
-bis_jobs = []
-
-with open('input/now.csv', 'r', encoding='utf-8') as csv_file:
+with open('input/clean/now.csv', 'r', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
         now_jobs.append(row)
 print("Loaded {} NOW jobs.".format(len(now_jobs)))
 
-with open('input/bis.csv', 'r', encoding='utf-8') as csv_file:
+bis_jobs = []
+with open('input/clean/bis.csv', 'r', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
         bis_jobs.append(row)
+print("Loaded {} BIS jobs.".format(len(bis_jobs)))
+# END OPENDATA
 
+# count processed jobs
+processed_jobs = 0
+processed_data = [['Dataset', 'Job', 'Cost', 'SQFT', 'Applicant', 'BIN']]
+# count the index errors
+index_error_count = 0
+index_error_rows = []
 
-
-quit()
-with open(csv_path, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        jobs_list.append(row[2])
-
-print('Gathered {} job numbers.'.format(len(jobs_list)))
-input()
-
-for job in jobs_list:
+for job in internal_jobs_list:
     try:
         if job[0].isalpha():
             #this is a now job
-            print('job number: ', job)
-            data = job_data.get_data('now', job)
-            
+            for row in now_jobs:
+                fixed_job_number =  job + "-I1"
+                if fixed_job_number.lower() == row[0].lower():
+                    dob = 'now'
+                    cost = row[31]
+                    sqft = row[32]
+                    applicant = row[12]
+                    bin = row[7]
+                    processed_data.append([dob, fixed_job_number, cost, sqft, applicant, bin])
         else:
-            #this is a bis job
-            data = job_data.get_data('bis', job)
-        
-        if (data):
-            gathered_jobs_list.append(data)
-        
-        if (len(gathered_jobs_list) % 1000 == 0):
-            with open('output/output.csv', 'w', newline='') as csv_file:
-                # Create a CSV writer object
-                print('Writing file...')
-                csv_writer = csv.writer(csv_file)
-                for row in gathered_jobs_list:
-                    csv_writer.writerow(row)
+            pass
+            for row in bis_jobs:
+                if job.lower() == row[0]:
+                    dob = 'bis'
+                    cost = row[46]
+                    sqft = row[83]
+                    applicant = row[38]
+                    bin = row[7]
+                    processed_data.append([dob, job, cost, sqft, applicant, bin])
+
+        processed_jobs += 1
+        if (processed_jobs % 1000 == 0):
+            print('...processed 1000 jobs. Continuing on the rest..')
 
     except IndexError:
-        pass
+        index_error_count += 1
+        index_error_rows.append(job)
 
+print('Index errors encountered:', index_error_count)
+print('Internal jobs processed:', processed_jobs)
 
+now = datetime.datetime.now()
+output_filename = 'output/'
+output_filename += now.strftime('%Y-%m-%d_%H-%M-%S.csv')
+with open(output_filename, 'w', newline='') as file:
+    writer = csv.writer(file)
+    for row in processed_data:
+        writer.writerow(row)
 
-    
+print('{} file written successfully'.format(output_filename))
